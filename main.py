edba746046de94ac7672bf42835b76f11b62764f
qsy123-coder <2175075194@qq.com>
2026-07-06 21:44:07 +0800

    添加Tavily搜索工具并更新依赖项

--- main.py
+++ main.py
@@ -3,28 +3,31 @@ from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 from langchain.tools import tool
+from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 import os
 load_dotenv()
 
-@tool
-def search (query:str) ->   str:
-    """
-    Tool that searches over Internet
-    Args:
-        query:The query to search for
-    Returns:
-        The search results
-    """
-    print(f"search for ${query}")
-    return "Tokyo's weather is sunny"
+tavily=TavilyClient()
+# @tool
+# def search (query:str) ->   str:
+#     """
+#     Tool that searches over Internet
+#     Args:
+#         query:The query to search for
+#     Returns:
+#         The search results
+#     """
+#     print(f"search for ${query}")
+#     return tavily.search(query=query)
 
-llm = ChatOpenAI()
-tools=[search]
+llm = ChatOpenAI(temperature=0,base_url="http://api.deepseek.com/v1",api_key=os.getenv("DEEPSEEK_API_KEY"),model="deepseek-chat")
+tools=[TavilySearch()]
 agent =create_agent(model=llm,tools=tools)
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages":HumanMessage(content="What the weather in tokyo?")})
+    result = agent.invoke({"messages":HumanMessage(content="帮我在linkedin上找几个洛杉矶ai工程师的岗位，并列出它们的一些细节?")})
     print (result)
 if __name__ == "__main__":
     main()
