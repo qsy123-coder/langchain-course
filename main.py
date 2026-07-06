fb86997fe9a4a63119a3118de911616d96f7fe64
qsy123-coder <2175075194@qq.com>
2026-07-06 20:24:23 +0800

    添加搜索工具并整合到代理中

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from langchain.tools import tool
+import os
+load_dotenv()
+
+@tool
+def search (query:str) ->   str:
+    """
+    Tool that searches over Internet
+    Args:
+        query:The query to search for
+    Returns:
+        The search results
+    """
+    print(f"search for ${query}")
+    return "Tokyo's weather is sunny"
+
+llm = ChatOpenAI()
+tools=[search]
+agent =create_agent(model=llm,tools=tools)
+
 def main():
     print("Hello from langchain-course!")
-
-
+    result = agent.invoke({"messages":HumanMessage(content="What the weather in tokyo?")})
+    print (result)
 if __name__ == "__main__":
     main()
